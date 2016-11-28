54fc013111839fc38eead796a99b27020a75ce23
Wendys Nieves <wendysnieves@gmail.com>
2016-11-27 19:38:23 -0500

    Se habilita repositorios de JPA para datatables.

--- src/main/java/com/insoftar/AppministratorApplication.java
+++ src/main/java/com/insoftar/AppministratorApplication.java
@@ -2,11 +2,14 @@ package com.insoftar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
 public class AppministratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppministratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppministratorApplication.class, args);
+    }
 }
